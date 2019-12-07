6b87c4382e85f0782fa470151404d21b67337cfb
lukasbauer <lukas.bauer.4@student.fhws.de>
2019-12-07 21:19:11 +0100

    day 7 part 1 finished. modified day 5 part 2 to accept multi inputs

--- day/7/1.js
+++ day/7/1.js
@@ -1,22 +1,40 @@
-const inputs = [0, 1, 2]
-// [1, 0, 2, 3, 4]
-// [0, 2, 3, 4, 1]
-// [2, 3, 4, 1, 0]
-// [3, 4, 1, 0, 2]
-// [4, 1, 0, 2, 4]
+const inputs = [0, 1, 2, 3, 4];
+var day5_2 = require("../5/2.js");
 
-module.exports.main = function (input) {
+module.exports.main = function(input) {
+  let prog = input
+    .trim()
+    .split(",")
+    .map(x => parseInt(x));
 
-}
-
-const allPossibilities = (arr) => {
-    if (arr.length > 1) {
-        allPossibilities(arr.slice(- arr.length + 1));
+  const all_combi = perm(inputs);
+  let all_signals = [];
+  for (let i = 0; i < all_combi.length; i++) {
+    let signal_in = 0;
+    for (let n = 0; n < all_combi[i].length; n++) {
+      signal_in = day5_2.intProgInterpreter(
+        [...prog],
+        [all_combi[i][n], signal_in]
+      ).output[0];
     }
-    arr.shift(1);
-}
+    all_signals.push(signal_in);
+  }
+  return all_signals.sort(function(a, b) {
+    return b - a;
+  });
+};
 
-const shift = (arr) => {
-    const first = arr.shift();
-    arr.push(first);
-}
+// this function was stolen from stackoverflow
+const perm = a => {
+  if (a.length) {
+    return a.reduce(
+      (r, v, i) => [
+        ...r,
+        ...perm([...a.slice(0, i), ...a.slice(i + 1)]).map(x => [v, ...x])
+      ],
+      []
+    );
+  } else {
+    return [[]];
+  }
+};
